7f896e5e1e69820969b95bcec62a307546041883
moreno <moreno.marrara@stud.fh-campuswien.ac.at>
2025-04-17 22:59:50 +0200

    MM20250417 modeled dynamo db data bases to match with the old ones.

---
+++ AWSLambda/database/CarsTable.py
@@ -0,0 +1,50 @@
+class Cars:
+    def __init__(self, dyn_resource):
+        """
+        :param dyn_resource: A Boto3 DynamoDB resource.
+        """
+        self.dyn_resource = dyn_resource
+        # The table variable is set during the scenario in the call to
+        # 'exists' if the table exists. Otherwise, it is set by 'create_table'.
+        self.table = None
+
+
+    def create_table(self, table_name):
+        """
+        Creates an Amazon DynamoDB table that can be used to store movie data.
+        The table uses the release year of the movie as the partition key and the
+        title as the sort key.
+
+        :param table_name: The name of the table to create.
+        :return: The newly created table.
+        """
+        try:
+            self.table = self.dyn_resource.create_table(
+                TableName=table_name,
+                KeySchema=[
+                    {"AttributeName": "id", "KeyType": "HASH"},  # Partition key
+                    {"AttributeName": "title", "KeyType": "RANGE"},  # Sort key
+                ],
+                AttributeDefinitions=[
+                    {"AttributeName": "id", "AttributeType": "N"},
+                    {"AttributeName": "name", "AttributeType": "S"},
+                    {"AttributeName": "model", "AttributeType": "S"},
+                    {"AttributeName": "price_per_day", "AttributeType": "N"},
+                    {"AttributeName": "is_available", "AttributeType": "BOOL"},
+                    {"AttributeName": "latitude", "AttributeType": "N"},
+                    {"AttributeName": "longitude", "AttributeType": "N"}
+                ],
+                BillingMode='PAY_PER_REQUEST',
+            )
+            self.table.wait_until_exists()
+        except ClientError as err:
+            logger.error(
+                "Couldn't create table %s. Here's why: %s: %s",
+                table_name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return self.table
+
